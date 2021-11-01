cb415d4facf421e34c0bd8e162391e7bb72fa5db
unknown <vodetisrikhar.reddy@gmail.com>
2021-10-31 19:17:35 -0700

    added booking hotels functionalities

--- src/app/features/hotels/hotels.module.ts
+++ src/app/features/hotels/hotels.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HotelsRoutingModule } from './hotels-routing.module';
 import { HotelsComponent } from './hotels.component';
 
+import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [HotelsComponent],
   imports: [
     CommonModule,
-    HotelsRoutingModule
+    HotelsRoutingModule,
+    ButtonModule
   ]
 })
 export class HotelsModule { }
