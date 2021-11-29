d18014018e7da0cb90d1be85071ae95982aae45e
unknown <vodetisrikhar.reddy@gmail.com>
2021-11-28 23:37:36 -0700

    US-79 T-81 added events component

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -17,20 +17,3 @@ export class AdminComponent implements OnInit {
   }
 
 }
-
-/*import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
-*/
