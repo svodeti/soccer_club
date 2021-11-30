5be41556104ac32f21265c24a3ef80ec43b670c9
unknown <vodetisrikhar.reddy@gmail.com>
2021-11-29 17:28:21 -0700

    US42 T76 added scheduling logic to tournament admin

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -8,12 +8,37 @@ import { Router } from '@angular/router';
 })
 export class AdminComponent implements OnInit {
 
+  schedule:any=[];
+  teams:any = [];
+  locations:any = [];
+  location:any;
+  dateTime:any ;
+  teamA:any;
+  teamB:any;
+  count:number = 0;
   constructor(private router: Router) {
-
+    this.teams = [
+      { name: 'team-1'},
+      { name: 'team-2'}
+    ]
+    this.locations = [
+      { name: 'location-1'},
+      { name: 'location-2'}
+    ]
    }
 
   ngOnInit(): void {
     //this.router.navigateByUrl('/login')
+  }
+  addSchedule(){
+    if(this.teamA && this.teamB && this.dateTime && this.location){
+      this.count++;
+      let x = { no:this.count, teamA : this.teamA,teamB: this.teamB, time:this.dateTime, location:this.location}
+      this.schedule.push(x);
+    }
+    else{
+      alert("Fields cannot be empty")
+    }
   }
 
 }
