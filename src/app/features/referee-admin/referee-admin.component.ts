ab34016200a6816c015659ca233e4ef459bbc4f0
unknown <vodetisrikhar.reddy@gmail.com>
2021-11-29 17:49:54 -0700

    US41 T77 added scheduling logic to referee-admin page

--- src/app/features/referee-admin/referee-admin.component.ts
+++ src/app/features/referee-admin/referee-admin.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./referee-admin.component.scss']
 })
 export class RefereeAdminComponent implements OnInit {
+  schedule:any = [];
+  count:number = 0;
+  referee:any;
+  referees:any = [];
+  dateTime;
+  locations:any = [];
+  location;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.referees = [
+      {name: 'referee-1'},
+      { name: 'referee-2'}
+    ];
+    this.locations = [
+      { name: 'location-1'},
+      { name: 'location-2'}
+    ]
   }
 
+  addSchedule(){
+    if(this.referee && this.dateTime && this.location){
+      this.count++;
+      let x = { no:this.count, name : this.referee, time:this.dateTime, location:this.location}
+      this.schedule.push(x);
+    }
+    else{
+      alert("Fields cannot be empty")
+    }
+  }
 }
